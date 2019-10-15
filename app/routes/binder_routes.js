3d6be8fa45033ff78294934d2cbb60e5d117c5f1
nicholaspowel <nicholaspowel@gmail.com>
2019-10-15 15:07:07 -0400

    Rename collection to binder

--- app/routes/binder_routes.js
+++ app/routes/binder_routes.js
@@ -3,10 +3,10 @@ const express = require('express')
 // Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
 
-// pull in Mongoose model for collections
-const Collection = require('../models/collection')
+// pull in Mongoose model for binders
+const Binder = require('../models/binder')
 
-// this is a collection of methods that help us detect situations when we need
+// this is a binder of methods that help us detect situations when we need
 // to throw a custom error
 const customErrors = require('../../lib/custom_errors')
 
@@ -17,7 +17,7 @@ const handle404 = customErrors.handle404
 const requireOwnership = customErrors.requireOwnership
 
 // this is middleware that will remove blank fields from `req.body`, e.g.
-// { collection: { title: '', text: 'foo' } } -> { collection: { text: 'foo' } }
+// { binder: { title: '', text: 'foo' } } -> { binder: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
@@ -28,43 +28,43 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // INDEX
-// GET /collections
-router.get('/collections', requireToken, (req, res, next) => {
-  Collection.find()
-    .then(collections => {
-      // `collections` will be an array of Mongoose documents
+// GET /binders
+router.get('/binders', requireToken, (req, res, next) => {
+  Binder.find()
+    .then(binders => {
+      // `binders` will be an array of Mongoose documents
       // we want to convert each one to a POJO, so we use `.map` to
       // apply `.toObject` to each one
-      return collections.map(collection => collection.toObject())
+      return binders.map(binder => binder.toObject())
     })
-    // respond with status 200 and JSON of the collections
-    .then(collections => res.status(200).json({ collections: collections }))
+    // respond with status 200 and JSON of the binders
+    .then(binders => res.status(200).json({ binders: binders }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
-// GET /collections/5a7db6c74d55bc51bdf39793
-router.get('/collections/:id', requireToken, (req, res, next) => {
+// GET /binders/5a7db6c74d55bc51bdf39793
+router.get('/binders/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
-  Collection.findById(req.params.id)
+  Binder.findById(req.params.id)
     .then(handle404)
-    // if `findById` is succesful, respond with 200 and "collection" JSON
-    .then(collection => res.status(200).json({ collection: collection.toObject() }))
+    // if `findById` is succesful, respond with 200 and "binder" JSON
+    .then(binder => res.status(200).json({ binder: binder.toObject() }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // CREATE
-// POST /collections
-router.post('/collections', requireToken, (req, res, next) => {
-  // set owner of new collection to be current user
-  req.body.collection.owner = req.user.id
+// POST /binders
+router.post('/binders', requireToken, (req, res, next) => {
+  // set owner of new binder to be current user
+  req.body.binder.owner = req.user.id
 
-  Collection.create(req.body.collection)
-    // respond to succesful `create` with status 201 and JSON of new "collection"
-    .then(collection => {
-      res.status(201).json({ collection: collection.toObject() })
+  Binder.create(req.body.binder)
+    // respond to succesful `create` with status 201 and JSON of new "binder"
+    .then(binder => {
+      res.status(201).json({ binder: binder.toObject() })
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -73,21 +73,21 @@ router.post('/collections', requireToken, (req, res, next) => {
 })
 
 // UPDATE
-// PATCH /collections/5a7db6c74d55bc51bdf39793
-router.patch('/collections/:id', requireToken, removeBlanks, (req, res, next) => {
+// PATCH /binders/5a7db6c74d55bc51bdf39793
+router.patch('/binders/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  delete req.body.collection.owner
+  delete req.body.binder.owner
 
-  Collection.findById(req.params.id)
+  Binder.findById(req.params.id)
     .then(handle404)
-    .then(collection => {
+    .then(binder => {
       // pass the `req` object and the Mongoose record to `requireOwnership`
       // it will throw an error if the current user isn't the owner
-      requireOwnership(req, collection)
+      requireOwnership(req, binder)
 
       // pass the result of Mongoose's `.update` to the next `.then`
-      return collection.updateOne(req.body.collection)
+      return binder.updateOne(req.body.binder)
     })
     // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
@@ -96,15 +96,15 @@ router.patch('/collections/:id', requireToken, removeBlanks, (req, res, next) =>
 })
 
 // DESTROY
-// DELETE /collections/5a7db6c74d55bc51bdf39793
-router.delete('/collections/:id', requireToken, (req, res, next) => {
-  Collection.findById(req.params.id)
+// DELETE /binders/5a7db6c74d55bc51bdf39793
+router.delete('/binders/:id', requireToken, (req, res, next) => {
+  Binder.findById(req.params.id)
     .then(handle404)
-    .then(collection => {
-      // throw an error if current user doesn't own `collection`
-      requireOwnership(req, collection)
-      // delete the collection ONLY IF the above didn't throw
-      collection.deleteOne()
+    .then(binder => {
+      // throw an error if current user doesn't own `binder`
+      requireOwnership(req, binder)
+      // delete the binder ONLY IF the above didn't throw
+      binder.deleteOne()
     })
     // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
